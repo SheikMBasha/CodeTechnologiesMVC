4e6f6074205a728293485107cf3e1e731ac86ecd
SheikMBasha <basha.mjcet@gmail.com>
2014-01-28 21:13:09 +0530

    Added Crud Operations and pages for institutes

---
+++ CodeTechnologiesMVC/Controllers/InstituteController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeTechnologiesMVC.Controllers
+{
+    public class InstituteController : Controller
+    {
+        //
+        // GET: /Institute/
+
+        public ActionResult Index()
+        {
+            using (var db = new sadiqEntities2())
+            {
+                List<institute> institutelist = db.institutes.ToList();
+                return View(institutelist);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(institute iObj)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                db.institutes.Add(iObj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                var iObj = db.institutes.Find(id);
+                return View(iObj);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                institute iObj = db.institutes.Where(i => i.Id == id).SingleOrDefault();
+                return View(iObj);
+             
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(institute iObj)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                institute localIObj = db.institutes.Where(i => i.Id == iObj.Id).SingleOrDefault();
+                if (localIObj != null)
+                {
+                    db.Entry(localIObj).CurrentValues.SetValues(iObj);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                institute iObj = db.institutes.Where(i => i.Id == id).SingleOrDefault();
+                db.institutes.Remove(iObj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}

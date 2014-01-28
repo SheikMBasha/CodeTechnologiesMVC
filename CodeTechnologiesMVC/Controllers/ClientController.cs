3722b3148eb72733f063b848f8090a36031eb6c2
SheikMBasha <basha.mjcet@gmail.com>
2014-01-28 23:50:39 +0530

    Added Client Crud operations with screens

---
+++ CodeTechnologiesMVC/Controllers/ClientController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeTechnologiesMVC.Controllers
+{
+    public class ClientController : Controller
+    {
+        //
+        // GET: /Client/
+
+        public ActionResult Index()
+        {
+            using (var db = new sadiqEntities2())
+            {
+                List<client> clientObj = db.GetAllClients();
+                return View(clientObj);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(client clientObj)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                db.clients.Add(clientObj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Details(string id)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                var clientObj = db.clients.Find(id);
+                return View(clientObj);
+            }
+        }
+
+        public ActionResult Delete(string id)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                var clientObj = db.clients.Where(i => String.Equals(i.Id, id)).SingleOrDefault();
+                db.clients.Remove(clientObj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string id)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                var clientObj = db.clients.Where(i => String.Equals(i.Id, id)).SingleOrDefault();
+                return View(clientObj);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(client clientObj)
+        {
+            using (var db = new sadiqEntities2())
+            {
+                client localClientObj = db.clients.Where(i => String.Equals(i.Id, clientObj.Id)).SingleOrDefault();
+                db.Entry(localClientObj).CurrentValues.SetValues(clientObj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}

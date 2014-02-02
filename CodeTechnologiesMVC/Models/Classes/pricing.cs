6fc127f4e203821b1d8ca7412986289bf6c909d1
SheikMBasha <basha.mjcet@gmail.com>
2014-02-03 00:07:00 +0530

    Added Validations to SellingPriceModule by creating partial class for POCO classes

---
+++ CodeTechnologiesMVC/Models/Classes/pricing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeTechnologiesMVC
+{
+    [MetadataType(typeof(PricingMetaData))]
+    public partial class pricing
+    {
+    }
+
+    public class PricingMetaData
+    {
+        [Required(ErrorMessage = "Institute Id is required!!")]
+        public Nullable<int> InstituteId { get; set; }
+        [Required(ErrorMessage="Client Id is required!!")]
+        public string ClientId { get; set; }
+        [Required(ErrorMessage="Voucher Type Required")]
+        public string VoucherType { get; set; }
+    }
+}
